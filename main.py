45aa1b6933fe0d1ea8dcb5168a8300fbc35b3c43
JTamarit <fjtamarit@gmail.com>
2020-08-27 12:36:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ import pygame, sys
 
 class Game():
     runners=[] 
-    _starLine = 20
-    _finishLine = 620 
+    __starLine = 20
+    __finishLine = 620 
     
     def __init__(self):
-        self._screen = pygame.display.set_mode((640,480))
-        self._background=pygame.image.load("images/background.png")
-        pygame.dsiplay.ser_caption ("CARRERA DE BICHOS")
+        self.__screen = pygame.display.set_mode((640,480))
+        self.__background=pygame.image.load("images/background.png")
+        pygame.display.set_caption("CARRERA DE BICHOS")
         
-        
+    def competir(self):
+        gameOver=False
+        while not gameOver:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameOVer = True
+            
+            self.__screen.blit(self.__background, (0,0))
+            pygame.display.flip()
+            
+        pygame.quit
+        sys.exit()
     
     
     
-if __name__='__main__':
+if __name__=='__main__':
+    
     game = Game()
+    pygame.font.init()
+    game.competir()
     
