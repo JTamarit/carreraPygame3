f978e1bed6792031d9ef9a839feb32ca643423de
JTamarit <fjtamarit@gmail.com>
2020-08-28 18:41:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ class Runner():
     
     def __init__(self, x=0, y=0):
         self.custome =pygame.image.load("images/turtle.png")
-        self.position =(x, y)
+        self.position =[x, y]
         self.name ="Tortuga"
+    
+    def avanzar(self):
+        self.position[0] += random.randint(1,6)
+        
         
     
     
 
 class Game():
+    
     runners=[] 
-    __starLine = 20
+    __startLine = -10
     __finishLine = 620 
     
     def __init__(self):
@@ -21,17 +26,35 @@ class Game():
         self.__background=pygame.image.load("images/background.png")
         pygame.display.set_caption("CARRERA DE BICHOS")
         
+        firstRunner = Runner(self.__startLine, 240)
+        firstRunner.name= "Speedy"
+        self.runners.append(firstRunner)
+        
+        
     def competir(self):
         gameOver=False
+        
         while not gameOver:
+            
             for event in pygame.event.get():
+                
                 if event.type == pygame.QUIT:
+                    
                     gameOVer = True
             
+            self.runners[0].avanzar()
+            if self.runners[0].position[0] >= self.__finishLine:
+                print("{} ha ganado".format(self.runners[0].name))
+                gameOver = True
+                
+            
             self.__screen.blit(self.__background, (0,0))
+            self.__screen.blit(self.runners[0].custome, self.runners[0].position)
+            
+            
             pygame.display.flip()
             
-        pygame.quit
+        pygame.quit()
         sys.exit()
     
     
